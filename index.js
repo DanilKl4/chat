6d1d99e19b0db495ae36f648fa47cda3891b50db
Данил Клочков <daniil.kl4@mail.ru>
2021-10-30 03:42:56 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+let express = require('express');
+let app = express();
+let server = require('http').createServer(app);
+let io = require('socket.io')(server);
+
+server.listen(8000);
+users = [];
+connections = [];
+
+app.get('/', function(request, response) {
+    response.sendFile(__dirname + '/index.html');
+})
+
+io.sockets.on('connection', function(socket){
+    console.log('Соединение прошло успешно!');
+    connections.push(socket);
+    
+    socket.on('disconnect', function(data){
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('Пользователь отключился')
+    });
+
+    socket.on('send msg', function(data, name) {
+        io.sockets.emit('add msg', data, name);
+    })
+});
